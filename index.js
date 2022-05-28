ec1b78874d085e1ac1b1ab8d292c8396dfe7ef11
SunjatunRuna <sunjatuna@gmail.com>
2022-05-28 15:31:34 +0600

    create admin api

--- index.js
+++ index.js
@@ -12,8 +12,23 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dqge6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+async function run() {
+    try {
         await client.connect();
         const serviceCollection = client.db('manufacturerWebsite').collection('services');
         const orderCollection = client.db('manufacturerWebsite').collection('order');
@@ -25,43 +40,63 @@ async function run(){
             const services = await cursor.toArray();
             res.send(services);
         });
-        app.get('/service/:id', async (req, res) =>{
+        app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        });
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const request = req.decoded.email;
+            const requestAccount = await userCollection.findOne({ email: request })
+            if (requestAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                }
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else{
+                res.status(403).send({message: 'Forbidden Access'})
+            }
         });
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
-            const filter = {email: email};
-            const options = {upsert: true};
+            const filter = { email: email };
+            const options = { upsert: true };
             const updateDoc = {
                 $set: user,
             }
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'})
-            res.send(result, token);
-        })
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
+            res.send({ result, token });
+        });
 
-        app.get('/order', async(req, res) =>{
+        app.get('/order', async (req, res) => {
             const buyer = req.query.buyer;
-            const query = {buyer: buyer};
+            const query = { buyer: buyer };
             const order = await orderCollection.find(query).toArray();
             res.send(order);
-
         })
         app.post('/order', async (req, res) => {
             const order = req.body;
-            const query = {name: order.order, quantity: order.quantity, buyer: order.buyer}
-
+            const query = { name: order.order, quantity: order.quantity, buyer: order.buyer }
             const result = await orderCollection.insertOne(order);
             res.send(result);
-          });
-      
+        });
+
     }
-    finally{
+    finally {
 
     }
 }
@@ -71,6 +106,6 @@ app.get('/', (req, res) => {
     res.send('server working')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('listening', port)
 })
